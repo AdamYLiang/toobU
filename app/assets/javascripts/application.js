567572607a1c6f1f25ca17dec65bc4fc665b81a8
adam <ayl292@nyu.edu>
2019-01-18 09:36:05 -0500

    Server and frontend start

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+// require turbolinks
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
+
---
+++ frontend/toobu.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById('root');
+    ReactDOM.render( <h1>toobU</h1>, rootEl );
+})
