ee4bb31cbc4f32ec042ca289249786482bca0475
adam <ayl292@nyu.edu>
2019-01-22 12:02:52 -0500

    Add auth controllers and views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,22 +1,21 @@
 class Api::SessionsController < ApplicationController
+    before_action :ensure_logged_in, only: [:destroy]
+
 
   def create
-    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    @user = User.find_by_credentials(user_params)
     if @user
       login!(@user)
       render 'api/users/show'
     else
-      render json: ["Invalid credentials"], status: 401
+      render json: ["Invalid username or password"], status: 422
     end
   end
 
   def destroy
-    if current_user
-      logout! 
-      render json: {} 
-    else
-      render json: ['No current user'], status: 404 
-    end
+    @user = current_user
+    logout! 
+    render 'api/users/show' 
   end
 
   private
