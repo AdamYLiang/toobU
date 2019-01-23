f89b3db50451bf11a2bc38317be1e66c4bc84c18
adam <ayl292@nyu.edu>
2019-01-23 12:38:02 -0500

    SVG Logo

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import Logo from './logo';
 
 class Nav extends React.Component {
 
     render() {
         return (
             <nav className="top-nav-bar">
-                <h1>toobU</h1>
+                <Logo />
                 <h1>SEARCH FILLER</h1>
                 <GreetingContainer />
             </nav>
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -5,12 +5,45 @@ class SessionForm extends React.Component {
         super(props);
         this.state = this.props.user;
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.demoLoginHelper = this.demoLoginHelper.bind(this);
+        this.demoLogin = this.demoLogin.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
         this.props.submitForm(this.state);
+    }
+
+    demoLogin(e) {
+        e.preventDefault();
+        const username = 'TestUser'.split('');
+        const password = '111111'.split('');
+        const submit = document.getElementById('session-submit');
+        this.setState({ username: '', password: '' }, () => {
+            this.demoLoginHelper(username, password, submit);
+        });
+    }
+
+    demoLoginHelper(username, password, submit) {
+        if (username.length > 0) {
+            this.setState(
+                { username: this.state.username + username.shift() }, () => {
+                    window.setTimeout(() =>
+                        this.demoLoginHelper(username, password, submit), 50);
+                }
+            );
+        } 
+        else if (password.length > 0) {
+            this.setState(
+                { password: this.state.password + password.shift() }, () => {
+                    window.setTimeout(() =>
+                        this.demoLoginHelper(username, password, submit), 50);
+                }
+            );
+        } else {
+            submit.click();
+        }
     }
 
     update(field) {
@@ -36,8 +69,12 @@ class SessionForm extends React.Component {
     }
 
     render() {
-        const typeHeader = (this.props.formType === 'login' ? 'Sign In' : 'Sign Up');
+        const typeHeader = (this.props.formType === 'login') ? 'Sign In' : 'Sign Up';
+        const demoButton = (this.props.formType === 'login') 
+            ? <button onClick={this.demoLogin} className="demo-btn">Demo user</button>
+            : ""
         let emailInput;
+
         if (this.props.formType === 'signup') {
             emailInput = (
                 <>
@@ -78,9 +115,11 @@ class SessionForm extends React.Component {
 
                     <div className ="login-signup-form-bottom">
                         <h2>{this.props.otherLink}</h2>
-                        <button onClick={this.handleSubmit}>{typeHeader}</button>
+                        <button onClick={this.handleSubmit} id="session-submit">{typeHeader}</button>
                     </div>
                 </form>
+
+                {demoButton}
 
             </div>
         )
