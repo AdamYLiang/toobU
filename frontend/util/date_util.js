2bf5f81ba590ff00eda2cd2712b5cd3e13a71e22
adam <ayl292@nyu.edu>
2019-01-31 13:55:55 -0500

    Fix date_util

--- frontend/util/date_util.js
+++ frontend/util/date_util.js
@@ -11,9 +11,14 @@ export const formatTimeAgo = createdTime => {
         } else {
             const days = Math.floor(hours / 24); 
             if(days < 7) {
-                return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+                return `${days} day${days > 1 ? "s" : ""} ago`;
             } else {
-                return `Too long ago`;
+                const weeks = Math.floor(days / 7);
+                if (weeks < 4) {
+                    return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+                } else {
+                    return `Too long ago`;
+                }
             }
         }
     }
