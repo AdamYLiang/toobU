997e76340568097ec99335fd09c9b28987e37e56
adam <ayl292@nyu.edu>
2019-02-21 20:27:42 -0500

    Like controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -2,14 +2,44 @@ class Api::LikesController < ApplicationController
     before_action :ensure_logged_in, only: [:create, :destroy]
 
     def create
-        
+        #find by and then create 
+        #pass up the like and maybe the differential ofr the bar 
+        #check the redux state after this and we're good!
+        @like = Like.find_by(
+            user_id: params[:like][:user_id],
+            likeable_id: params[:like][:likeable_id],
+            likeable_type: params[:like][:likeable_type]
+        )
+
+        unless @like
+            @like = Like.new(like_params)
+            @like.user_id = current_user.id
+        end
+
+        @like.thumbs_up = params[:like][:thumbs_up]
+
+        if @like.save 
+            render :show
+        end
     end
 
     def destroy
+        @like = Like.find_by(
+            user_id: params[:like][:user_id],
+            likeable_id: params[:like][:likeable_id],
+            likeable_type: params[:like][:likeable_type]
+        )
+
+        if @like 
+            @like.destroy
+            render :show
+        else
+            render json: ["No Like Found"], status: 404
+        end
     end
 
     private
     def like_params
-        params.require(:like).permit(:user_id, :thumps_up, :likeable_type, :likeable_id)
+        params.require(:like).permit(:user_id, :thumbs_up, :likeable_type, :likeable_id)
     end
 end
