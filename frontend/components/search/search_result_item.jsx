59f16a87564de8dd6d10a85762eb093d92f1afbc
adam <ayl292@nyu.edu>
2019-03-24 21:38:31 -0400

    Search result item

---
+++ frontend/components/search/search_result_item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SearchResultItem extends React.Component {
+    render() {
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default SearchResultItem;
